9c786425129eb5d3e553972ffa9cd0371ede12bb
variables <variables@riseup.net>
2015-11-20 12:35:54 +0100

    View hinzugefügt

---
+++ src/main/java/view/ViewShop.java
@@ -0,0 +1,48 @@
+package view;
+
+import javafx.geometry.Orientation;
+import javafx.scene.layout.BorderPane;
+
+public class ViewShop extends BorderPane {
+
+	public ViewShop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Orientation getContentBias() {
+		// TODO Auto-generated method stub
+		return super.getContentBias();
+	}
+
+	@Override
+	protected double computeMinWidth(double height) {
+		// TODO Auto-generated method stub
+		return super.computeMinWidth(height);
+	}
+
+	@Override
+	protected double computeMinHeight(double width) {
+		// TODO Auto-generated method stub
+		return super.computeMinHeight(width);
+	}
+
+	@Override
+	protected double computePrefWidth(double height) {
+		// TODO Auto-generated method stub
+		return super.computePrefWidth(height);
+	}
+
+	@Override
+	protected double computePrefHeight(double width) {
+		// TODO Auto-generated method stub
+		return super.computePrefHeight(width);
+	}
+
+	@Override
+	protected void layoutChildren() {
+		// TODO Auto-generated method stub
+		super.layoutChildren();
+	}
+
+}
