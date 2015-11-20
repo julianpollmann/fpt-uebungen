060a1e4662850f92ca11d25f938bfe40ad2b713a
Don_Anton <anton.faller@stud.uni-due.de>
2015-11-20 12:37:42 +0100

    nur Komments geloescht

--- src/main/java/model/ProductList.java
+++ src/main/java/model/ProductList.java
@@ -9,35 +9,26 @@ public class ProductList extends ArrayList<Product> implements fpt.com.ProductLi
 
 	@Override
 	public boolean delete(Product product) {
-		// TODO Auto-generated method stub
 		return this.remove(product);
 	}
 
 	@Override
 	public Product findProductById(long id) {
 		for (Product prod : this) {
-
 			if (prod.getId() == id) {
-
 				return prod;
-
 			}
-
 		}
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Product findProductByName(String name) {
 		for (Product prod : this) {
-
 			if (prod.getName().equals(name)) {
-
 				return prod;
 			}
 		}
-		// TODO Auto-generated method stub
 		return null;
 	}
 
