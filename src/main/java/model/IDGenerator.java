70dc9fc3a3a314fe7051ccb04106acac178ced8f
variables <variables@riseup.net>
2015-12-09 17:56:28 +0100

    IDGenerator Default Iterator Wert zurückgesetzt

--- src/main/java/model/IDGenerator.java
+++ src/main/java/model/IDGenerator.java
@@ -6,7 +6,7 @@ import exceptions.IdOverflowException;
 public class IDGenerator {
 
 	public static long generateID(ProductList pList) throws IdOverflowException {
-		for (int i=1; i<2; i++) {
+		for (int i=1; i<1000000; i++) {
 			if (pList.findProductById(i) == null) {
 				System.out.print(" " +i);
 				return i;
