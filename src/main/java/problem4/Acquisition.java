d8515fcd52975b5040d1073981aec83c032cd7ad
sfanfall <sfanfall@github.com>
2015-12-31 14:48:08 +0100

    Anfang UEB4
    Einfach nur ein package mit zwei leeren Runnable-Klassen

---
+++ src/main/java/problem4/Acquisition.java
@@ -0,0 +1,11 @@
+package problem4;
+
+public class Acquisition implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/problem4/Cashpoint.java
@@ -0,0 +1,11 @@
+package problem4;
+
+public class Cashpoint implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
