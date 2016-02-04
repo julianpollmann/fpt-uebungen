edfad4c1db9184a34ee05392d8b026a43986704a
BananexTwin <benjamin.gansert@stud.uni-due.de>
2016-02-04 23:20:01 +0100

    UDP Kram - nur noch GUI Kram fehlt
    Und wir sollten ViewCustomer noch mit nem Thread starten...

---
+++ src/main/java/warehouse/UDPClientThread.java
@@ -0,0 +1,79 @@
+package warehouse;
+
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+
+public class UDPClientThread extends Thread {
+
+	private DatagramPacket packet;
+	private DatagramSocket socket;
+
+	public UDPClientThread(DatagramPacket packet, DatagramSocket socket)
+			throws SocketException {
+		this.packet = packet;
+		this.socket = socket;
+	}
+
+	public void run() {
+		// Daten auslesen
+		InetAddress address = packet.getAddress();
+		int port = packet.getPort();
+		int len = packet.getLength();
+		byte[] data = packet.getData();
+
+		System.out.printf("Anfrage von %s vom Port %d mit der Länge %d:%n%s%n",
+				address, port, len, new String(data, 0, len));
+
+		// Nutzdaten in ein Stringobjekt  übergeben und nach "time" suchen
+		String da = new String(packet.getData());
+		Scanner sc = new Scanner(da);
+		String keyword = sc.findInLine("time");
+
+		if (keyword.equals("time")) {
+
+			// Datum abrufen, ins gewünschte Format bringen und in Byte-Sequenz umwandeln
+			Date dt = new Date();
+			SimpleDateFormat sdt = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+			String dt2 = sdt.format(dt);
+
+			byte[] myDate = dt2.toString().getBytes();
+
+			// Paket mit neuen Daten (Datum) als Antwort vorbereiten
+			packet = new DatagramPacket(myDate, myDate.length, address, port);
+
+			try {
+				// Paket versenden
+				socket.send(packet);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		} else {
+			byte[] myDate = new byte[1024];
+			myDate = new String("Command unknown").getBytes();
+			try {
+				sleep(5000);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+			// Paket mit Information, dass das Schl�sselwort ung�ltig ist als
+			// Antwort vorbereiten
+			packet = new DatagramPacket(myDate, myDate.length, address, port);
+			try {
+				// Paket versenden
+				socket.send(packet);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		sc.close();
+	}
+}
--- src/main/java/warehouse/UDPServer.java
+++ src/main/java/warehouse/UDPServer.java
@@ -9,14 +9,14 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class UDPServer {
-	
-	
+
+
 
 	public static void main(String[] args) {
 		// Socket erstellen unter dem der Server erreichbar ist
 		DatagramSocket socket = null;
 		try {
-			socket = new DatagramSocket(3431);
+			socket = new DatagramSocket(6667);
 			while (true) {
 				// Neues Paket anlegen
 				DatagramPacket packet = new DatagramPacket(new byte[5], 5);
@@ -24,7 +24,7 @@ public class UDPServer {
 				try {
 					socket.receive(packet);
 					// Empfangendes Paket in einem neuen Thread abarbeiten
-					new ClientThread(packet, socket).start();
+					new UDPClientThread(packet, socket).start();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
@@ -37,69 +37,4 @@ public class UDPServer {
 
 	}
 
-}
-
-class ClientThread extends Thread {
-
-	private DatagramPacket packet;
-	private DatagramSocket socket;
-
-	public ClientThread(DatagramPacket packet, DatagramSocket socket)
-			throws SocketException {
-		this.packet = packet;
-		this.socket = socket;
-	}
-
-	public void run() {
-		// Daten auslesen
-		InetAddress address = packet.getAddress();
-		int port = packet.getPort();
-		int len = packet.getLength();
-		byte[] data = packet.getData();
-
-		System.out.printf("Anfrage von %s vom Port %d mit der L�nge %d:%n%s%n",
-				address, port, len, new String(data, 0, len));
-
-		// Nutzdaten in ein Stringobjekt �bergeben
-		String da = new String(packet.getData());
-		// Kommandos sollen durch : getrennt werden
-		Scanner sc = new Scanner(da).useDelimiter(":");
-		// Erstes Kommando filtern
-		String keyword = sc.next();
-
-		if (keyword.equals("DATE")) {
-
-			Date dt = new Date();
-			byte[] myDate = dt.toString().getBytes();
-
-			// Paket mit neuen Daten (Datum) als Antwort vorbereiten
-			packet = new DatagramPacket(myDate, myDate.length, address, port);
-
-			try {
-				// Paket versenden
-				socket.send(packet);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-		} else {
-			byte[] myDate = new byte[1024];
-			myDate = new String("Command unknown").getBytes();
-			try {
-				sleep(2000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-			// Paket mit Information, dass das Schl�sselwort ung�ltig ist als
-			// Antwort vorbereiten
-			packet = new DatagramPacket(myDate, myDate.length, address, port);
-			try {
-				// Paket versenden
-				socket.send(packet);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		sc.close();
-	}
 }
